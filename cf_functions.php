ae7c120f832678bb266e4c2f41c2b7ccd5328302
Markus Santanen <markus@santmark.fi>
2015-10-18 19:55:02 +0300

    full use case implemented

--- cf_functions.php
+++ cf_functions.php
@@ -1,6 +1,15 @@
 <?php
 
-include("config.php");
+include("config.php"); // get auth and domain data
+
+function getPublicIP() {
+  $ip_json = json_decode(file_get_contents("https://api.ipify.org/?format=json"), true);
+
+  if ($ip_json != null  && !filter_var($ip_json["ip"], FILTER_VALIDATE_IP) === false)
+    return $ip_json["ip"];
+  else
+    return null;
+}
 
 function cfGetDomainData($domain) {
 
@@ -16,6 +25,28 @@ function cfGetDomainData($domain) {
   return httpPost($data);
 }
 
+function setDomainARecord($domain, $recid, $ip, $name) {
+
+  global $config_auth;
+
+  $data = array(
+    'a' => "rec_edit",
+    'tkn' => $config_auth['tkn'],
+    'email' => $config_auth['email'],
+    'z' => $domain,
+    'type' => "A",
+    'id' => $recid,
+    'content' => $ip,
+    'ttl' => "600",
+    'name' => $name,
+    'service_mode' => "1"
+
+  );
+
+  return httpPost($data);
+}
+
+
 function httpPost($data) {
 
   global $cfUrl;
@@ -29,7 +60,7 @@ function httpPost($data) {
 
   $output = curl_exec($ch);
   curl_close($ch);
-  return json_decode($output);
+  return json_decode($output, true);
 }
 
 ?>
--- cloudflare.php
+++ cloudflare.php
@@ -4,9 +4,53 @@ include("config.php"); // get auth and domain data
 include("cf_functions.php");
 include("jsonformat.php");
 
-$output = cfGetDomainData($config_domains[0]);
+$publicIP = getPublicIP();
+if ($publicIP != null)
+  echo ("\nThis server's current public IP is: " . $publicIP . "\n");
+else {
+  echo "\nCould not retrieve public IP. Exiting...\n";
+  exit;
+}
 
-// printf(json_format($output));
-print_r($output);
+foreach ($config_domains as $domain) {
 
+  // get DNS records for domain
+  $domainData = cfGetDomainData($domain);
+
+  if ($domainData["result"] != "success") {
+    echo ("\nFailed to get domain data. Exiting....\n");
+    exit;
+  }
+
+  // loop through DNS records to retrieve A-record
+  foreach ($domainData["response"]["recs"]["objs"] as $entry) {
+    if ( $entry["type"] == "A" ) {
+      $cf_ip = $entry["content"];
+      $cf_recid = $entry["rec_id"];
+      $cf_name = $entry["name"];
+      break;
+    }
+  }
+
+  // printf(json_format($output));
+  // print_r($domainData);
+  echo ("\n" . $domain . ":");
+  echo ("\n\tIP: " . $cf_ip);
+  echo ("\n\tRec_id: " . $cf_recid);
+
+  echo ("\n\tAction needed: ");
+  if ($publicIP == $cf_ip)
+    echo ("none\n");
+  else {
+    echo ("updating... ");
+    $updateResult = setDomainARecord($domain, $cf_recid, $publicIP, $cf_name);
+    if ($updateResult["result"] != "success") {
+      echo ("failed to update domain data. Exiting....\n");
+      exit;
+    }
+    else {
+      echo ("done!\n");
+    }
+  }
+}
 ?>
